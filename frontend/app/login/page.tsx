e7acd4668ddec4250abbfa2dba8a07e9aaca5ec7
Khoi Ly <lhkhoi95@gmail.com>
2023-08-28 23:10:09 -0700

    fix CartItem component

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,7 +1,16 @@
+"use client"
 import { GoogleSignInButton } from "../components/GoogleSignInButton";
 import { CredentialsForm } from "../components/LoginForm";
+import { redirect } from 'next/navigation'
+import { useVinaTeaStore } from "../store/store";
 
 export default function Login() {
+  const user = useVinaTeaStore((state) => {
+
+    return state.user;
+  });
+  if (user) redirect('/');
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-[#1F2937] dark:border-gray-700">
